d94b6000d991730705d74bf2aabe2cb88416d3a4
antwan hommy <antwan.hommy@gmail.com>
2021-01-19 13:12:40 +0200

    backend to heroku

---
+++ puhelinluettelo/backend/build/precache-manifest.5f2351d9e72d411c55e0a0764802485c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5aeaeafb3b851a621d53a412287df4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0184c852ecf602045a",
+    "url": "/static/css/main.394db26b.chunk.css"
+  },
+  {
+    "revision": "8f4f45adc222c096b17f",
+    "url": "/static/js/2.a03bcb81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a03bcb81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0184c852ecf602045a",
+    "url": "/static/js/main.5959ac50.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
